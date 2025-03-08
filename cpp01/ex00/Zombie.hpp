9b7fe954f510315b7f2cdddb2a57a937df3de3b7
Jek John Tan <jetan@u91z02s01.42kl.edu.my>
2025-03-08 16:06:53 +0800

    add header and class

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -6,7 +6,26 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 21:46:17 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/07 21:47:01 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/08 16:05:56 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie
+{
+private:
+	std::string name;
+public:
+	Zombie(std::string name);
+	~Zombie();
+	void announce( void );
+};
+
+Zombie* newZombie( std::string name );
+void randomChump( std::string name );
+
+#endif
