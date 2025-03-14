e005927235a1307ab82eae66e68b007e36fd288e
Jek John Tan <jetan@u91z01s04.42kl.edu.my>
2025-03-14 15:18:43 +0800

    add default constructor and implement weapon's method

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/14 14:20:36 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/14 15:17:41 by jetan            ###   ########.fr       */
+/*   Created: 2025/03/14 14:56:52 by jetan             #+#    #+#             */
+/*   Updated: 2025/03/14 15:17:34 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WEAPON_HPP
-# define WEAPON_HPP
+#include "Weapon.hpp"
 
-#include <string>
-
-class Weapon
+Weapon::Weapon(): type("")
 {
-	private:
-		std::string type;
-	public:
-		Weapon();
-		Weapon(std::string type);
-		~Weapon();
-		//setter function
-		void setType(std::string type);
-		//getter function
-		std::string getType() const;
-};
+}
 
-#endif
+Weapon::Weapon(std::string type): type(type)
+{	
+}
+
+Weapon::~Weapon()
+{
+}
+
+void Weapon::setType(std::string type)
+{
+	this->type = type;
+}
+
+std::string Weapon::getType() const { return type; }
