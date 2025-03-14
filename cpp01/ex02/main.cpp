c52b211cbd0da59ad7afaed8d7c11e77946d0567
Jek John Tan <jetan@u91z01s04.42kl.edu.my>
2025-03-14 14:17:49 +0800

    implement print memory address and value

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,15 +6,24 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 13:45:05 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/14 14:03:48 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/14 14:16:29 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
 
 int main()
 {
 	std::string str = "HI THIS IS BRAIN";
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
+	//memory address
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	//value
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 }
