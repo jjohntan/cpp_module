ba1fbc1b3f6dee30abb0ef4e71e49edef6c9bb8c
Jek John Tan <jetan@u91z01s04.42kl.edu.my>
2025-03-14 14:40:32 +0800

    add Weapon class

---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 14:20:36 by jetan             #+#    #+#             */
+/*   Updated: 2025/03/14 14:39:51 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+#include <string>
+
+class Weapon
+{
+	private:
+		std::string type;
+	public:
+		Weapon(std::string type);
+		~Weapon();
+		//setter function
+		void setType(std::string type);
+		//getter function
+		std::string getType();
+};
+
+#endif
