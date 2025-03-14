c29f88bae77258d45fcc204c970b91decadd5372
Jek John Tan <jetan@u91z01s04.42kl.edu.my>
2025-03-14 17:12:33 +0800

    implement HumanA and change variable name in HumanA class

---
+++ cpp01/ex03/HumanA.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 15:33:50 by jetan             #+#    #+#             */
+/*   Updated: 2025/03/14 16:55:35 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "Weapon.hpp"
+#include <iostream>
+
+HumanA::HumanA(std::string name, Weapon& weapon): name(name), _Weapon(weapon)
+{
+}
+
+HumanA::~HumanA()
+{
+}
+
+void HumanA::attack()
+{
+	std::cout << this->name << " attacks with their " << _Weapon.getType() << std::endl;
+}
---
+++ cpp01/ex03/HumanA.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 15:20:04 by jetan             #+#    #+#             */
+/*   Updated: 2025/03/14 16:52:45 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+#include "Weapon.hpp"
+#include <string>
+
+class HumanA
+{
+	private:
+		Weapon& _Weapon;
+		std::string name;
+	public:
+		HumanA(std::string name, Weapon& weapon);
+		~HumanA();
+		void attack();
+};
+
+#endif
