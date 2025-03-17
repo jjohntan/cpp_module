60424ffa87608cec9ca793388a815e2aafd25e36
Jek John Tan <jetan@u91z05s03.42kl.edu.my>
2025-03-17 14:08:43 +0800

    add class HumanB

---
+++ cpp01/ex03/HumanB.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 17:16:03 by jetan             #+#    #+#             */
+/*   Updated: 2025/03/17 14:00:30 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+#include "Weapon.hpp"
+#include <string>
+
+class HumanB
+{
+	private:
+		std::string name;
+		Weapon* _Weapon;
+	public:
+		HumanB(std::string name);
+		~HumanB();
+		void attack();
+		void setWeapon(Weapon& _Weapon);
+};
+
+#endif
